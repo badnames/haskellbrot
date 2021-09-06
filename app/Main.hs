19e1b114caa3614c7796c3d751ab069ce6597f61
Moritz Schalk <schalk.moritz@tutanota.de>
2021-09-06 18:01:38 +0200

    mend

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,12 @@ module Main where
 import Lib
 import Data.Complex
 
-isInMandelbrotSet :: Complex Double -> Complex Double -> Int -> Bool
-isInMandelbrotSet c z 0 = True
+
+
+isInMandelbrotSet :: Complex Double -> Complex Double -> Int -> Int
+isInMandelbrotSet c z 0 = 0
 isInMandelbrotSet c z itterations | (magnitude z)  < 4 = isInMandelbrotSet c (z**2 + c) (itterations - 1)
-                             | (magnitude z) >= 4 = False
+                                  | (magnitude z) >= 4 = itterations
 
 main :: IO ()
 main = do
