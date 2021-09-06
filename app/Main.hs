3c5e21eefd36a49155de66f9bc6ee2b789cbee3b
Moritz Schalk <schalk.moritz@tutanota.de>
2021-09-06 19:13:54 +0200

    Cleaned the code up a bit, mainly by extracting constants.

--- app/Main.hs
+++ app/Main.hs
@@ -4,17 +4,24 @@ import Lib
 import Data.Complex
 import Graphics.Image
 
+width = 750
+height = 500
+scaleDivisor = 250
+
+xOffset = 2
+yOffset = 1
+
 isInMandelbrotSet :: Complex Double -> Complex Double -> Int -> Int
 isInMandelbrotSet c z 0 = 0
 isInMandelbrotSet c z itterations | (Data.Complex.magnitude z)  < 4 = isInMandelbrotSet c (z**2 + c) (itterations - 1)
                                   | (Data.Complex.magnitude z) >= 4 = itterations
 
 calculatePixel :: Int -> Int -> Pixel RGB Double
-calculatePixel x y = PixelRGB  0 0 value where
-    real = (fromIntegral y / 250) - 2
-    imag = (fromIntegral x / 250) - 1
+calculatePixel y x = PixelRGB  0 0 value where
+    real = (fromIntegral x / scaleDivisor) - xOffset
+    imag = (fromIntegral y / scaleDivisor) - yOffset
     value = ((fromIntegral (isInMandelbrotSet (real :+ imag) 0 1000)) / 1000)
 
 main = do
-    let image = makeImageR VS (500, 750) (\(x, y) -> calculatePixel x y)
+    let image = makeImageR VS (height, width) (\(y, x) -> calculatePixel y x)
     writeImageExact PNG [] "output.png" image
